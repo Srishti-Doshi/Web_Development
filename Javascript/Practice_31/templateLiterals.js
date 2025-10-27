7fa8ef03de3ee50773dd6c46014b0e4f4c1ae8ed
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-10-27 20:43:11 +0530

    Template Literals

---
+++ Javascript/Practice_31/templateLiterals.js
@@ -0,0 +1,7 @@
+let realName = "Srishti";
+let nickName = "Tinka";
+
+console.log("My real name is "+realName+" and I also have a nick name as "+nickName);
+
+//Template Literals
+console.log(`My real name is ${realName} and I also have a nick name as ${nickName}`)
