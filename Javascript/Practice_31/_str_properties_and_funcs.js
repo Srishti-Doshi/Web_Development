70a97e3cc1fa1a980505a8bf02dc38f29cce272f
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-10-29 22:55:17 +0530

    String: properties and functions

---
+++ Javascript/Practice_31/_str_properties_and_funcs.js
@@ -0,0 +1,15 @@
+//Strings -> Immutable
+a = "Apple"
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(a.length)
+console.log(a.slice(1,3))
+console.log(a.replace("p",'12'))
+b = ' Shake'
+console.log(a.concat(b, " Healthy"))
+console.log(a+b)
+
+x = "      Puuung   "
+console.log(x.trim()) //Remove Whitespaces
+
+//There are many functions of string explore them on console by inspecting any website.
