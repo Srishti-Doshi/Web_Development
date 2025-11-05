d76591276c103290c6f46acc49a7da6af67f504c
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-11-05 09:57:37 +0530

    array: splice

---
+++ Javascript/Practice_33/arrSplice.js
@@ -0,0 +1,7 @@
+//splice change the existing array
+let numbers = [1, 2, 3, 4, 5, 6, 7]
+console.log(numbers.splice(1,3))
+console.log(numbers)
+
+console.log(numbers.splice(1,2, 444, 56))
+console.log(numbers)
