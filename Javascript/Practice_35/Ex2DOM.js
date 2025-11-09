503ba9de9b0ad12098b699f9ae9385cb090da77c
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-11-09 09:25:48 +0530

    practice js

---
+++ Javascript/Practice_35/Ex2DOM.js
@@ -0,0 +1,8 @@
+// views -> integer , 727000 -> 727k , 7270000 -> 727M
+// duration -> string
+
+function createCard(title, cName, views, monthsOld, duration){
+
+}
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
