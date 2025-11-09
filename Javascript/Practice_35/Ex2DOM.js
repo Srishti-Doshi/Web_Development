3e20a209fcda83a2047509fbf3e611cbbbeb0a3a
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-11-09 21:32:01 +0530

    Dynamic Website Builder

--- Javascript/Practice_35/Ex2DOM.js
+++ Javascript/Practice_35/Ex2DOM.js
@@ -1,8 +1,38 @@
 // views -> integer , 727000 -> 727k , 7270000 -> 727M
 // duration -> string
 
-function createCard(title, cName, views, monthsOld, duration){
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+    let viewstr;
+    if(views <1000)
+    {
+        viewstr = views;
+    }
+    else if(views > 1000000)
+    {
+        viewstr = views/1000000 + "M";
+    }
+    else{
+        viewstr = views/1000 + "K";
+    }
+    let html = `<div class="container">
+        <div class="card">
+            <div class="image">
+                <img src="${thumbnail}"
+                    alt="">
+                <div class="capsule">${duration}</div>
+            </div>
+
+            <div class="text">
+                <h1>${title}</h1>
+                <p>${cName} . ${views} views . ${monthsOld} months ago</p>
+            </div>
+        </div>
+    </div>`;
+
+    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html;
 
 }
 
-createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw");
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw");
