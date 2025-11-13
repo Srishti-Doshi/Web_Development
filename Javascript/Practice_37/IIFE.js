4cd6f22deeb788c69cbbab6c8628f3b66453b1d0
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-11-13 22:59:09 +0530

    IIFE: Immediately Invoked Function Expression

---
+++ Javascript/Practice_37/IIFE.js
@@ -0,0 +1,28 @@
+function getData(dataId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data", dataId);
+            resolve("success");
+        }, 4000)
+    });
+}
+
+// async function getAllData(){
+//     console.log("getting data1.....");
+//     await getData(1);
+//     console.log("getting data2.....");
+//     await getData(2);
+//     console.log("getting data3.....");
+//     await getData(3);
+// }
+
+// getAllData();
+
+(async function (){
+    console.log("getting data1.....");
+    await getData(1);
+    console.log("getting data2.....");
+    await getData(2);
+    console.log("getting data3.....");
+    await getData(3);
+})();
