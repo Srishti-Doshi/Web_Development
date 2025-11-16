cab39ce4238c9d86a405c6843d4bcf12cba4e8ec
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-11-17 01:16:47 +0530

    Hacker's Terminal: Create a hacking simulator

---
+++ Javascript/Practice_43/Ex_14.js
@@ -0,0 +1,71 @@
+let txt = document.querySelector(".txt");
+
+setTimeout(() => {
+    txt.innerHTML = "Initializing Hacking.  ";
+}, 1000)
+setTimeout(() => {
+    txt.innerHTML = "Initializing Hacking..  ";
+}, 1500)
+setTimeout(() => {
+    txt.innerHTML = "Initializing Hacking...  ";
+}, 2000)
+setTimeout(() => {
+    txt.innerHTML = "Initializing Hacking.  ";
+}, 2500)
+setTimeout(() => {
+    txt.innerHTML = "Initializing Hacking..  ";
+}, 3000)
+setTimeout(() => {
+    txt.innerHTML = "Initializing Hacking...  ";
+}, 3500)
+
+setTimeout(() => {
+    txt.innerHTML = "Reading your files.";
+}, 6000);
+setTimeout(() => {
+    txt.innerHTML = "Reading your files..";
+}, 6500);
+setTimeout(() => {
+    txt.innerHTML = "Reading your files...";
+}, 7000);
+
+setTimeout(() => {
+    txt.innerHTML = "Password files detected.";
+}, 8000);
+setTimeout(() => {
+    txt.innerHTML = "Password files detected..";
+}, 8200);
+setTimeout(() => {
+    txt.innerHTML = "Password files detected...";
+}, 8500);
+
+setTimeout(() => {
+    txt.innerHTML = "Sending all passwords and personal files to server. .";
+}, 9000);
+setTimeout(() => {
+    txt.innerHTML = "Sending all passwords and personal files to server...";
+}, 10000);
+
+setTimeout(() => {
+    txt.innerHTML = "Cleaning up...";
+}, 10500);
+setTimeout(() => {
+    txt.innerHTML = "Cleaning up .";
+}, 11000);
+setTimeout(() => {
+    txt.innerHTML = "Cleaning up ..";
+}, 11200);
+setTimeout(() => {
+    txt.innerHTML = "Cleaning up...";
+}, 11500);
+setTimeout(() => {
+    txt.innerHTML = "Cleaning up.";
+}, 11600);
+setTimeout(() => {
+    txt.innerHTML = "Cleaning up..";
+}, 11700);
+setTimeout(() => {
+    txt.innerHTML = "Cleaning up...";
+}, 11900);
+
+
