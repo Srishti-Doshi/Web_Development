6e93e4de70a7f4dee78c752fe6c3bfb3684ac151
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-02-27 05:57:49 +0530

    react update array of objects

--- React/10-react-state-updates/src/App.jsx
+++ React/10-react-state-updates/src/App.jsx
@@ -1,6 +1,7 @@
 import MyComponent from './MyComponent.jsx'
 import MyComponent2 from './MyComponent2.jsx'
 import MyComponent3 from './MyComponent3.jsx'
+import MyComponent4 from './MyComponent4.jsx'
 
 function App(){
   return(
@@ -14,6 +15,10 @@ function App(){
     <hr />
 
     <MyComponent3/>
+
+    <hr />
+
+    <MyComponent4/>
     </>
   );
 }
