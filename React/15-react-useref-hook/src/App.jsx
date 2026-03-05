2e8316f4cd0190ed22fbdb71dada1abb12d64aa2
Srishti-Doshi <srishtidoshi7@gmail.com>
2026-03-05 22:39:09 +0530

    react useref hook

---
+++ React/15-react-useref-hook/src/App.jsx
@@ -0,0 +1,21 @@
+/*
+useState() = Re-renders the component when the state value changes.
+
+useRef() = "use Reference" does not cause re-renders when its value changes. When you want a component to "remember" some information, but you don't want that information to trigger new renders.
+
+        1. Accessing/Interacting with DOM elements
+        2. Handling Focus, Animations, and Transitions
+        3. Managing Timers and Intervals
+*/
+
+import MyComponent from "./MyComponent";
+
+function App(){
+    return(
+        <>
+            <MyComponent></MyComponent>
+        </>
+    );
+}
+
+export default App;
